6db23dffce4c0f4844d1e3a4dd73a52b7c551680
Andrés Ortega <betoandriu01@gmail.com>
2023-10-22 17:49:40 -0500

    Adición de nuevas rutas para publicación de capas en geoserver

--- controllers/geoserver.controller.js
+++ controllers/geoserver.controller.js
@@ -5,12 +5,187 @@ const { Layer } = require('../database');
 
 const GeoServerController = {};
 
+GeoServerController.createDataStore = (req, res) => {
+  Layer.findOne({
+    where: { external_id: req.params.id }, 
+  }).then(layer => {
+    if(!layer.publish) {
+      instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/datastores`, {
+        dataStore: {
+          name: layer.name,
+          connectionParameters: {
+            entry: [
+              {
+                "@key":"url",
+                "$":`file://${process.env.ROOT_SHP}/${layer.folderpath}`
+              }
+            ],        
+          },
+        }
+      }).then(response => {
+        return res.status(200).send(response.data);
+      }).catch(err => {
+        console.log(err);
+        return res.status(500).send({ message: 'An error occurred in the request.', error: err.data });
+      });
+    } else {
+      return res.status(400).send({ message: 'La capa ya fue publicada.' });
+    }
+  }).catch(err => {
+    console.log(err);
+    return res.status(500).send({ message: 'An error occurred in the request.' });
+  });
+}
+
+GeoServerController.createShapeLayer = (req, res) => {
+  Layer.findOne({
+    where: { external_id: req.params.id }, 
+  }).then(layer => {
+    if(!layer.publish) {
+      let filename = layer.filename.split(".")[0];
+      instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/datastores/${layer.name}/featuretypes`, {
+        featureType: {
+          name: layer.external_id,
+          nativeName: filename,
+        }
+      }).then(async (response) => {
+        try {
+          layer.published = true;
+          await layer.save();
+        } catch (error) {
+          console.log("ERROR AL ACTUALIZAR CAPA LOCAL", error);
+        }
+
+        return res.status(200).send(response.data);
+      }).catch(err => {
+        console.log(err);
+        return res.status(500).send({ message: 'An error occurred in the request.', error: err });
+      });
+
+    } else {
+      return res.status(400).send({ message: 'La capa ya fue publicada.' });
+    }
+  }).catch(err => {
+    console.log(err);
+    return res.status(500).send({ message: 'An error occurred in the request.', error: err });
+  });
+}
+
+GeoServerController.addStyles = (req, res) => {
+  Layer.findOne({
+    where: { external_id: req.params.id }, 
+  }).then(layer => {    
+    if(layer.styles) {
+      let styles_uuid = layer.styles.split(".");
+      var stylesFile = null;
+
+      try {
+        stylesFile = fs.readFileSync(`${process.env.ROOT_SHP}/${styles_uuid[0]}/${layer.styles}`, 'utf-8');              
+      } catch (error) {
+        console.log(error);
+      }
+  
+      if(stylesFile) {
+        instance.post(`/styles?name=${styles_uuid[0]}_style`, stylesFile, {
+          headers: {
+            'Content-Type': 'application/vnd.ogc.se+xml'
+          }
+        }).then(response => {
+            instance.post(`/layers/${layer.external_id}/styles?default=true`, {
+              style: {
+                name: `${styles_uuid[0]}_style`,
+              },
+            }).then(response => {
+              return res.status(200).send(response.data);
+            }).catch(err => {
+              return res.status(500).send({ message: 'Error al aplicar los estilos a la capa.', error: err });
+            });
+        }).catch(err => {
+          return res.status(500).send({ message: 'Error al publicar los estilos.', error: err });
+        });
+      } else {
+        return res.status(400).send({ message: 'No se encontró el archivo de estilos.' });
+      }
+    } else {
+      return res.status(400).send({ message: 'La capa no tiene estilos adjuntos.' });
+    }
+  }).catch(err => {
+    console.log(err);
+    return res.status(500).send({ message: 'An error occurred in the request.', error: err });
+  });
+}
+
+GeoServerController.createCoverageStore = (req, res) => {
+  Layer.findOne({
+    where: { external_id: req.params.id }, 
+  }).then(layer => {
+    if(!layer.published) {
+      instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/coveragestores`, {
+        coverageStore: {
+          name: layer.name,
+          workspace: {
+            name: process.env.GEOSERVER_WSPACE
+          },
+          enabled: true,
+          type: 'GeoTIFF',
+          url: `file://${process.env.ROOT_SHP}/${layer.folderpath}/${layer.filename}`,
+        }
+      }).then(response => {        
+        return res.status(200).send(response.data);
+      }).catch(err => {
+        console.log(err);
+        return res.status(500).send({ 
+          message: 'An error occurred in the request.',
+          error: err,
+        });
+      });
+    } else {
+      return res.status(400).send({ message: 'La capa ya fue publicada.' });
+    }
+  }).catch(err => {
+    console.log(err);
+    return res.status(500).send({ message: 'An error occurred in the request.', error: err });
+  });
+}
+
+GeoServerController.createRasterLayer = (req, res) => {
+  Layer.findOne({
+    where: { external_id: req.params.id }, 
+  }).then(layer => {
+    let filename = layer.filename.split(".")[0];
+    instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/coveragestores/${layer.name}/coverages`, {
+      coverage: {
+        name: layer.external_id,
+        nativeName: filename,
+      }
+    }).then(async (response) => {
+      try {
+        layer.published = true;
+        await layer.save();
+      } catch (error) {
+        console.log("ERROR AL ACTUALIZAR CAPA LOCAL", error);
+      }
+      
+      return res.status(200).send(response.data);
+    }).catch(err => {
+      console.log(err);
+      return res.status(500).send({ 
+        message: 'An error occurred in the request.',
+        data: err.data,
+      });
+    });    
+  }).catch(err => {
+    console.log(err);
+    return res.status(500).send({ message: 'An error occurred in the request.', error: err });
+  });
+}
+
 GeoServerController.publishShape = (req, res) => {
   Layer.findOne({
     where: { id_layer: req.params.id }, 
   }).then(layer => {  
     if(!layer.published) {
-      instance.post('/workspaces/Ecuador/datastores', {
+      instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/datastores`, {
         dataStore: {
           name: layer.name,
           connectionParameters: {
@@ -24,7 +199,7 @@ GeoServerController.publishShape = (req, res) => {
         }
       }).then(response => {
         let filen = layer.filename.split(".")[0];
-        instance.post(`/workspaces/Ecuador/datastores/${layer.name}/featuretypes`, {
+        instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/datastores/${layer.name}/featuretypes`, {
           featureType: {
             name: layer.external_id,
             nativeName: filen,
@@ -33,9 +208,7 @@ GeoServerController.publishShape = (req, res) => {
           layer.published = true;
           await layer.save();
           if(layer.styles) {
-            let styles_name = layer.name.replace(/\s/g, "");
             let styles_uuid = layer.styles.split(".");
-
             var stylesFile = null
 
             try {
@@ -45,14 +218,14 @@ GeoServerController.publishShape = (req, res) => {
             } 
 
             if(stylesFile) {
-              instance.post(`/styles?name=${styles_name}_style`, stylesFile, {
+              instance.post(`/styles?name=${styles_uuid[0]}_style`, stylesFile, {
                 headers: {
                   'Content-Type': 'application/vnd.ogc.se+xml'
                 }
               }).then(response => {
                   instance.post(`/layers/${layer.external_id}/styles?default=true`, {
                     style: {
-                      name: `${styles_name}_style`,
+                      name: `${styles_uuid[0]}_style`,
                     },
                   }).then(response => {
                     console.log("**** ESTILOS PUBLICADOS ****")
@@ -97,11 +270,11 @@ GeoServerController.publishRaster = (req, res) => {
     where: { id_layer: req.params.id }, 
   }).then(layer => {
     if(!layer.published) {
-      instance.post('/workspaces/Ecuador/coveragestores', {
+      instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/coveragestores`, {
         coverageStore: {
           name: layer.name,
           workspace: {
-            name: 'Ecuador'
+            name: process.env.GEOSERVER_WSPACE
           },
           enabled: true,
           type: 'GeoTIFF',
@@ -109,7 +282,7 @@ GeoServerController.publishRaster = (req, res) => {
         }
       }).then(response => {
         let filen = layer.filename.split(".")[0];
-        instance.post(`/workspaces/Ecuador/coveragestores/${layer.name}/coverages`, {
+        instance.post(`/workspaces/${process.env.GEOSERVER_WSPACE}/coveragestores/${layer.name}/coverages`, {
           coverage: {
             name: layer.external_id,
             nativeName: filen,
--- routes/v1/geoserver.routes.js
+++ routes/v1/geoserver.routes.js
@@ -5,6 +5,13 @@ const Controller = require('../../controllers/geoserver.controller');
 const router = express.Router();
 const PATH = 'geoserver';
 
+router.post(`/${PATH}/create/datastore/:id`, ensureAuth, Controller.createDataStore);
+router.post(`/${PATH}/create/shape/:id`, ensureAuth, Controller.createShapeLayer);
+router.post(`/${PATH}/create/styles/:id`, ensureAuth, Controller.addStyles);
+
+router.post(`/${PATH}/create/coveragestore/:id`, ensureAuth, Controller.createCoverageStore);
+router.post(`/${PATH}/create/raster/:id`, ensureAuth, Controller.createRasterLayer);
+
 router.post(`/${PATH}/publish/shape/:id`, ensureAuth, Controller.publishShape);
 router.post(`/${PATH}/publish/raster/:id`, ensureAuth, Controller.publishRaster);
 
